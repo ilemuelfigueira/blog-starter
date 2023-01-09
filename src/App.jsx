4b359f47979d69f74807024a85cda943033b24a9
ilemuelfigueira <imarinaapereira@gmail.com>
2023-01-09 18:22:27 -0300

    feat: ajustando conteudo das noticias

--- src/App.jsx
+++ src/App.jsx
@@ -26,21 +26,21 @@ const Layout = styled.main`
 const noticias = [
   {
     "cdNoticia": 1,
-    "nmNoticia": "Noticia 1",
-    "deNoticia": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel ultricies lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia.",
-    "dtCadastro": "2021-03-01T00:00:00",
+    "nmNoticia": "Agora é oficial: o Windows 11 está vindo",
+    "deNoticia": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.",
+    "dtCadastro": "02 de jul, 2021",
   },
   {
     "cdNoticia": 2,
-    "nmNoticia": "Noticia 2",
-    "deNoticia": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel ultricies lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia.",
-    "dtCadastro": "2021-03-01T00:00:00",
+    "nmNoticia": "Tim Berners-Lee vai leiloar código-fonte da web",
+    "deNoticia": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.",
+    "dtCadastro": "02 de jul, 2021",
   },
   {
     "cdNoticia": 3,
-    "nmNoticia": "Noticia 3",
-    "deNoticia": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel ultricies lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eu lectus. Nullam auctor, nunc eget lacinia.",
-    "dtCadastro": "2021-03-01T00:00:00",
+    "nmNoticia": "Tem Firefox novo no pedaço e você vai querer migrar",
+    "deNoticia": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.",
+    "dtCadastro": "02 de jul, 2021",
   },
 ]
 
